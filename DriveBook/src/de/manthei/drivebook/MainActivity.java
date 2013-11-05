0f5e286cedd38c5a22c43fd1333679b42fddf478
pmanthei <marzocchi_bomber90@yahoo.de>
2013-11-05 22:01:39 +0100

    Add: if conditions with toast by failure

--- DriveBook/src/de/manthei/drivebook/MainActivity.java
+++ DriveBook/src/de/manthei/drivebook/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -29,9 +30,20 @@ public class MainActivity extends Activity {
 		EditText editCons = (EditText) findViewById(R.id.editCons);
 		EditText editCost = (EditText) findViewById(R.id.editCost);
 		
+		if (	isEmpty(editDist, this.getText(R.id.textDist).toString()) || 
+				isEmpty(editFuel, this.getText(R.id.textFuel).toString()) || 
+				isEmpty(editPrice, this.getText(R.id.textPrice).toString()) ) {
+			return;
+		}
+		
 		float distance = Float.parseFloat(editDist.getText().toString());
 		float fuel = Float.parseFloat(editFuel.getText().toString());
 		float price = Float.parseFloat(editPrice.getText().toString());
+		
+		if (fuel == 0 || distance == 0 || price == 0) {
+			Toast.makeText(this, "Felder dürfen nicht 0 sein.", Toast.LENGTH_SHORT).show();
+			return;
+		}
 		
 		float consume = (fuel / distance) * 100;
 		float cost = price / distance;
@@ -39,6 +51,15 @@ public class MainActivity extends Activity {
 		editCons.setText(Float.toString(consume));
 		editCost.setText(Float.toString(cost));
 		
+	}
+	
+	private boolean isEmpty(EditText editText, String fieldName) {
+		String text = editText.getText().toString();
+		if (text.isEmpty() || text.equals(".")) {
+			Toast.makeText(this, "Gefahrene Strecke ist leer!", Toast.LENGTH_SHORT).show();
+			return true;
+		}
+		return false;
 	}
 	
 }
