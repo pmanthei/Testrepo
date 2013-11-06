013639b0613ede08be507ca77ea9ba1c7fb686d0
pmanthei <marzocchi_bomber90@yahoo.de>
2013-11-06 22:48:11 +0100

    Add Testclass for CarJSONActivity

---
+++ DriveBookTest/src/de/manthei/drivebook/test/CarJSONActivityTest.java
@@ -0,0 +1,20 @@
+package de.manthei.drivebook.test;
+
+import android.test.ActivityUnitTestCase;
+import de.manthei.drivebook.CarJSONActivity;
+
+public class CarJSONActivityTest extends ActivityUnitTestCase<CarJSONActivity> {
+
+	public CarJSONActivityTest() {
+		super(CarJSONActivity.class);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
