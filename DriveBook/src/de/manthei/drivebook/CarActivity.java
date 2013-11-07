b977333fb5700e7f8639b475efbd12d4cc92f3f3
pmanthei <marzocchi_bomber90@yahoo.de>
2013-11-07 22:18:19 +0100

    Add: read JSON

--- DriveBook/src/de/manthei/drivebook/CarActivity.java
+++ DriveBook/src/de/manthei/drivebook/CarActivity.java
@@ -1,10 +1,19 @@
 package de.manthei.drivebook;
 
-import android.os.Bundle;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class CarActivity extends Activity {
 
@@ -14,6 +23,36 @@ public class CarActivity extends Activity {
 		setContentView(R.layout.activity_car);
 		// Show the Up button in the action bar.
 		setupActionBar();
+		
+		try {
+			FileInputStream inputStream = openFileInput(CarJSONActivity.FILE_NAME);
+			int size = inputStream.available();
+			byte[] buffer = new byte[size];
+			inputStream.read(buffer);
+			inputStream.close();
+			String json = new String(buffer);
+			JSONObject jsonObject = new JSONObject(json);
+			
+			TextView manufacturer = (TextView) findViewById(R.id.textInputCarManufacturer);
+			TextView model = (TextView) findViewById(R.id.textInputCarModel);
+			TextView engineSize = (TextView) findViewById(R.id.textInputCarEngineSize);
+			TextView power = (TextView) findViewById(R.id.textInputCarPower);
+			TextView vintage = (TextView) findViewById(R.id.textInputCarVintage);
+			
+			manufacturer.setText(jsonObject.getString(getString(R.string.text_manufacturer)));
+			model.setText(jsonObject.getString(getString(R.string.text_model)));
+			engineSize.setText(jsonObject.getString(getString(R.string.text_engine_size)));
+			power.setText(jsonObject.getString(getString(R.string.text_power)));
+			vintage.setText(jsonObject.getString(getString(R.string.text_vintage)));
+			
+		} catch (FileNotFoundException e) {
+			Toast.makeText(this, "Can't open file!", Toast.LENGTH_SHORT).show();
+		} catch (IOException e) {
+			Toast.makeText(this, "Can't read file!", Toast.LENGTH_SHORT).show();
+		} catch (JSONException e) {
+			Toast.makeText(this, "Can't create JSONObject!", Toast.LENGTH_SHORT).show();
+		}
+		
 	}
 
 	/**
